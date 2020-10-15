388cc8965882226d73da2869efff8556deb0ac46
Sosruko21 <69717549+Sosruko21@users.noreply.github.com>
2020-10-16 01:25:27 +0300

    Метод хорд и выбор программы
    Объединение методов решения трансцентдентных уравнений

--- transcendent.js
+++ transcendent.js
@@ -67,3 +67,69 @@ function dichotomy() {
     };
     console.log(results);
 };
+
+function hords() {
+    let i, j;
+    let mathFunctionStrParts = [];
+    window.mathFunction = [];
+    let eps = prompt('Введите необходимую точность вычислений');
+    let eps1 = eps + 1;
+    let eps2 = eps + 1;
+    let epsGen = eps + 1;
+    let results = [['a', 'c', 'b', 'f(a)', 'f(c)', 'f(b)'], []];
+
+    mathFunctionStrParts = (
+        prompt(
+            'Введите вашу функцию \nПример записи: 3*x*x*x +0.5*x*x -7\nВсе слагаемые разделяются пробелом\nЕсли коэффицент = 1, то его нужно указать\nПример: 1*x*x'
+        ).toString()).split(' ');
+    mathFunction = createMathFunction(mathFunctionStrParts);
+    results[1][0] = prompt('Введите число "а"');
+    results[1][2] = prompt('Введите число "b"');
+    results[1][3] = findPoint(results[1][0]);   /* a */
+    results[1][5] = findPoint(results[1][2]);   /* b */
+    results[1][1] = (results[1][0] * results[1][5] - results[1][2] * results[1][3]) / (results[1][5] - results[1][3]);
+    results[1][4] = findPoint(results[1][1]);   /* c */
+    for (j = 0; j < results[1].length; j++) {
+        results[1][j] = parseFloat(results[1][j]);
+    }
+    for (i = 2; epsGen > eps; i++) {
+        results.push([]);
+        if (results[i - 1][3] / results[i - 1][4] < 0) {
+            results[i][0] = results[i - 1][0];
+            results[i][2] = results[i - 1][1];
+        } else if (results[i - 1][4] / results[i - 1][5] < 0) {
+            results[i][0] = results[i - 1][1];
+            results[i][2] = results[i - 1][2];
+        };
+        results[i][3] = findPoint(results[i][0]);
+        results[i][5] = findPoint(results[i][2]);
+        results[i][1] = (results[i][0] * results[i][5] - results[i][2] * results[i][3]) / (results[i][5] - results[i][3]);
+        results[i][4] = findPoint(results[i][1]);
+        eps1 = Math.abs(results[i][1] - results[i - 1][1]);
+        eps2 = Math.abs(results[i][4]);
+        if (eps1 > eps2) {
+            epsGen = eps1;
+        } else {
+            epsGen = eps2;
+        }
+    };
+    console.log(results);
+};
+
+function transcendent() {
+    let question = parseInt(
+        prompt(
+            'Выберите номер программы\n1 - Метод половинных сечений\n2 - Метод хорд (секущих)'
+        )
+    );
+    switch (question) {
+        case 1:
+            dichotomy();
+            break;
+        case 2:
+            hords();
+            break;
+        default:
+            alert('Ты дурак?');
+    }
+}
